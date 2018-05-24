71b0aa18fd2330d639479e3c31dbe58d7e6d6387
superman-wrdh <13419655360@163.com>
2018-05-24 23:07:18 +0800

    some models

---
+++ web_api/web_stroage.py
@@ -0,0 +1,22 @@
+# -*- encoding: utf-8 -*-
+
+
+def upload_file():
+    """
+    上传文件 需要权限认证
+    """
+    pass
+
+
+def get_file():
+    """
+    根据文件id返回文件流  仅能查看不需要权限的文件
+    """
+    pass
+
+
+def load_file():
+    """
+    获取文件 管理员调用接口 查看全部文件
+    """
+    pass
