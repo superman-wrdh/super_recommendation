34f4f71bbffe38314e87604578517adafe0d60aa
superman-wrdh <13419655360@163.com>
2018-06-02 23:33:54 +0800

    多文件上传

--- web_api/urls.py
+++ web_api/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 from web_api import views
+from web_api import web_stroage
 
 urlpatterns = (
     url(r'^test', views.first_api),
     url(r'^user_reg', views.register),
-    url(r'^send_reg_code', views.send_reg_code)
+    url(r'^send_reg_code', views.send_reg_code),
+    url(r'^upload', web_stroage.upload_file)
 )
--- web_api/web_stroage.py
+++ web_api/web_stroage.py
@@ -1,11 +1,39 @@
 # -*- encoding: utf-8 -*-
+import os
+
+from rest_framework import permissions, status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+
+# Create your views here.
+from super_recommendation.settings import BASE_DIR
 
 
-def upload_file():
+@api_view(http_method_names=['POST', ])
+@permission_classes((permissions.AllowAny,))
+def upload_file(request):
     """
-    上传文件 需要权限认证
+    多文件上传文件
     """
-    pass
+    params = request.data
+    files = params.getlist('file', None)
+    names = []
+    resp = {'success': True}
+    try:
+        for file_obj in files:
+            mine_type = file_obj.content_type
+            file_name = file_obj.name
+            file_size = file_obj.size
+            f = open(os.path.join(BASE_DIR, "templates", "upload", file_obj.name), 'wb')
+            for i in file_obj.chunks():
+                f.write(i)
+            f.close()
+            print('文件名', file_name, '文件类型', mine_type, '文件大小', file_size, 'B')
+            names.append(file_name)
+        resp['name'] = names
+    except Exception as e:
+        resp.update({'success': False, 'msg': str(e)})
+    return Response(resp, status=status.HTTP_200_OK)
 
 
 def get_file():
